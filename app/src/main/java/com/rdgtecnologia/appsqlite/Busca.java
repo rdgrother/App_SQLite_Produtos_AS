eed4ca7602bfaf65df807b6cfa2a15713599dbd2
Rodrigo Rother <rdgtecno@gmail.com>
2020-10-01 19:07:54 -0300

    Enviando Primeiro

---
+++ app/src/main/java/com/rdgtecnologia/appsqlite/Busca.java
@@ -0,0 +1,14 @@
+package com.rdgtecnologia.appsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Busca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busca);
+    }
+}
---
+++ app/src/main/java/com/rdgtecnologia/appsqlite/MainActivity.java
@@ -0,0 +1,41 @@
+package com.rdgtecnologia.appsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCadastrar;
+    private Button btnBuscar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnBuscar =  findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Cadastro.class);
+                startActivity(intent);
+            }
+        });
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Busca.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
